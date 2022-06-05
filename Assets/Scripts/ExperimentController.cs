2faa0738c406e7c27e74fcd3d46555d8225c93f8
JonahEgashira <jonahega@gmail.com>
2022-06-05 14:43:17 +0900

    Shift right hand by 1.0 degree

--- Assets/Scripts/ExperimentController.cs
+++ Assets/Scripts/ExperimentController.cs
@@ -5,11 +5,14 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
 {
     
     private int _cubeState;
+    private int _touchedCount;
     private const int NumberOfCubeState = 4;
     public GameObject prefabCube;
-    public GameObject RightHand;
+    public GameObject rightHandAnchor;
+    public GameObject rightHand;
 
     private const double Degree = 30.0;
+    private const double ShiftAngle = Math.PI * 1.0 / 180.0;
     private const double HypotenuseLength = 0.3;
     private const float BaseX = 0.0f;
     private const float BaseY = 1.125f;
@@ -30,20 +33,39 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
         _frontCubePos = new Vector3(BaseX, BaseY, BaseZ);
         _rightCubePos = new Vector3(BaseX + (float)_xLength, BaseY, BaseZ + (float)_zLength);
         _leftCubePos = new Vector3(BaseX + (float)(-_xLength), BaseY, BaseZ + (float)_zLength);
-        
+    }
+
+    private void Update()
+    {
         ShiftRightHand();
     }
 
     private void ShiftRightHand()
     {
-        RightHand.transform.position += transform.up * 0.1f;
+        var turn = _touchedCount / NumberOfCubeState;
+        var angle = turn * ShiftAngle;
+
+        var zDistance = rightHand.transform.position.z - BaseZ;
+        var xAdjustment = zDistance * Math.Tan(angle);
+
+        var anchorPos = rightHandAnchor.transform.position;
+        switch (_cubeState)
+        {
+            case 0 or 3:
+                rightHand.transform.position = anchorPos + new Vector3((float)-xAdjustment,0, 0);
+                break;
+            case 1 or 2:
+                rightHand.transform.position = anchorPos + new Vector3((float)xAdjustment,0, 0);
+                break;
+        }
     }
 
     public void UpdateCubeState()
     {
+        _touchedCount += 1;
         _cubeState += 1;
         _cubeState %= NumberOfCubeState;
-        Invoke(nameof(GenerateCube), 0.2f);
+        Invoke(nameof(GenerateCube), 0.3f);
         Debug.Log("cubeState: " + _cubeState);
     }
 
