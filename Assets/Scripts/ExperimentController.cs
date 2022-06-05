33c948c15509de5880de63d83a0389cc595748af
JonahEgashira <jonahega@gmail.com>
2022-06-05 11:55:22 +0900

    Fix generated cube position

--- Assets/Scripts/ExperimentController.cs
+++ Assets/Scripts/ExperimentController.cs
@@ -8,11 +8,12 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
     private const int NumberOfCubeState = 4;
     public GameObject prefabCube;
 
-    private const double Angle = Math.PI * 30.0 / 180.0;
-    private const double HypotenuseLength = 10.0;
+    private const double Degree = 30.0;
+    private const double HypotenuseLength = 0.2;
     private const float BaseX = 0.0f;
     private const float BaseY = 1.125f;
     private const float BaseZ = 0.1f;
+    private double _angle;
     private double _xLength;
     private double _zLength;
 
@@ -22,8 +23,9 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
 
     private void Start()
     {
-        _xLength = HypotenuseLength * Math.Sin(Angle);
-        _zLength = HypotenuseLength * Math.Cos(Angle);
+        _angle = Math.PI * Degree / 180.0;
+        _xLength = HypotenuseLength * Math.Sin(_angle);
+        _zLength = HypotenuseLength * Math.Cos(_angle);
         _frontCubePos = new Vector3(BaseX, BaseY, BaseZ);
         _rightCubePos = new Vector3(BaseX + (float)_xLength, BaseY, BaseZ + (float)_zLength);
         _leftCubePos = new Vector3(BaseX + (float)(-_xLength), BaseY, BaseZ + (float)_zLength);
@@ -37,7 +39,6 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
         Debug.Log("cubeState: " + _cubeState);
     }
 
-    // TODO: Fix cube position
     private void GenerateCube()
     {
         var ballPos = _cubeState switch
