efaf702200d3f42ccde2f389ac592711265559f1
JonahEgashira <jonahega@gmail.com>
2022-06-06 19:57:34 +0900

    Iikanjini Fix

--- Assets/Scripts/ExperimentController.cs
+++ Assets/Scripts/ExperimentController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
@@ -6,6 +8,7 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
     
     private int _cubeState;
     private int _touchedCount;
+    private int _frameCount;
     private const int MaxTouchedCount = 120;
     private const int NumberOfCubeState = 4;
     public GameObject prefabCube;
@@ -13,11 +16,15 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
     public GameObject rightHand;
 
     private const double Degree = 30.0;
-    private const double ShiftAngle = Math.PI * 1.0 / 180.0;
-    private const double HypotenuseLength = 0.3;
-    private const float BaseX = 0.0f;
-    private const float BaseY = 1.125f;
-    private const float BaseZ = 0.1f;
+    private const double MaxAngle = Math.PI * 30.0 / 180.0;
+    private const double ShiftAngle = Math.PI * 0.5 / 180.0;
+    private const double HypotenuseLength = 0.25;
+    private const float CubeBaseX = 0.0f;
+    private const float CubeBaseY = 1.125f;
+    private const float CubeBaseZ = 0.10f;
+    
+    private float _forceFieldBaseZ;
+    
     private double _angle;
     private double _xLength;
     private double _zLength;
@@ -26,47 +33,70 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
     private Vector3 _rightCubePos;
     private Vector3 _leftCubePos;
 
+    private readonly List<string> _actualRightHandPositions = new();
+
     private void Start()
     {
+        _cubeState = 0;
+        _touchedCount = 0;
+        _frameCount = 0;
+        
         _angle = Math.PI * Degree / 180.0;
         _xLength = HypotenuseLength * Math.Sin(_angle);
         _zLength = HypotenuseLength * Math.Cos(_angle);
-        _frontCubePos = new Vector3(BaseX, BaseY, BaseZ);
-        _rightCubePos = new Vector3(BaseX + (float)_xLength, BaseY, BaseZ + (float)_zLength);
-        _leftCubePos = new Vector3(BaseX + (float)(-_xLength), BaseY, BaseZ + (float)_zLength);
+        _frontCubePos = new Vector3(CubeBaseX, CubeBaseY, CubeBaseZ);
+        _rightCubePos = new Vector3(CubeBaseX + (float)_xLength, CubeBaseY, CubeBaseZ + (float)_zLength);
+        _leftCubePos = new Vector3(CubeBaseX + (float)(-_xLength), CubeBaseY, CubeBaseZ + (float)_zLength);
     }
 
     private void Update()
     {
         ShiftRightHand();
+        _frameCount++;
+
+        if (_frameCount % 5 == 0)
+        {
+            PushRightHandPosition();
+        }
+
+        if (_touchedCount == MaxTouchedCount)
+        {
+            StoreResults();
+        }
     }
 
     private void ShiftRightHand()
     {
-        var turn = Math.Min(_touchedCount, 120) / NumberOfCubeState;
-        var angle = turn * ShiftAngle;
+        var turn = _touchedCount / NumberOfCubeState;
+        var angle = Math.Min(turn * ShiftAngle, MaxAngle);
 
-        var zDistance = rightHand.transform.position.z - BaseZ;
-        var xAdjustment = zDistance * Math.Tan(angle);
+        var actualPosition = rightHandAnchor.transform.position;
+        var virtualPosition = rightHand.transform.position;
+        var zDistance = actualPosition.z - _forceFieldBaseZ;
+        var xAdjustment = zDistance >= -0.1 ? zDistance * Math.Tan(angle) : 0.0;
 
-        var anchorPos = rightHandAnchor.transform.position;
-        switch (_cubeState)
+        virtualPosition = _cubeState switch
         {
-            case 0 or 3:
-                rightHand.transform.position = anchorPos + new Vector3((float)-xAdjustment,0, 0);
-                break;
-            case 1 or 2:
-                rightHand.transform.position = anchorPos + new Vector3((float)xAdjustment,0, 0);
-                break;
-        }
+            0 or 3 => actualPosition + new Vector3((float)-xAdjustment, 0, 0),
+            1 or 2 => actualPosition + new Vector3((float)xAdjustment, 0, 0),
+            _ => virtualPosition
+        };
+        rightHand.transform.position = virtualPosition;
     }
 
-    public void UpdateCubeState()
+    public void UpdateCubeAndFieldState()
     {
+        if (_cubeState is 0 or 2)
+        {
+            var position = rightHandAnchor.transform.position;
+            _forceFieldBaseZ = position.z;
+        }
+        
         _touchedCount += 1;
         _cubeState += 1;
         _cubeState %= NumberOfCubeState;
         Invoke(nameof(GenerateCube), 0.3f);
+        
         Debug.Log("cubeState: " + _cubeState);
     }
 
@@ -82,4 +112,23 @@ public class ExperimentController : SingletonMonoBehaviour<ExperimentController>
         };
         Instantiate(prefabCube, cubePos, Quaternion.identity);
     }
+
+    private void PushRightHandPosition()
+    {
+        var actualPosition = rightHandAnchor.transform.position;
+        _actualRightHandPositions.Add(_cubeState + "," + actualPosition.x + "," + actualPosition.y + "," + actualPosition.z);
+    }
+
+    private void StoreResults()
+    {
+        var dateStr = DateTime.Now.ToString("MM_dd_HH_mm");
+        var path = Application.dataPath + "/HandPositions/";
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        var filePath = path + dateStr + ".csv";
+        File.WriteAllLines(filePath, _actualRightHandPositions);
+    }
+    
 }
--- Assets/Scripts/TouchObjectScript.cs
+++ Assets/Scripts/TouchObjectScript.cs
@@ -1,15 +1,13 @@
+using System;
 using UnityEngine;
 
 public class TouchObjectScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Target") && other.enabled)
-        {
-            other.enabled = false;
-            Destroy(other.gameObject);
-            Debug.Log("Enter Called");
-            ExperimentController.Instance.UpdateCubeState();
-        }
+        if (!other.gameObject.CompareTag("Target") || !other.enabled) return;
+        other.enabled = false;
+        Destroy(other.gameObject);
+        ExperimentController.Instance.UpdateCubeAndFieldState();
     }
 }
